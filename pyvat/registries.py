e9505e75b4d598fd1bd98360df9585268c18a53d
Mike <558554+karpitsky@users.noreply.github.com>
2022-09-27 18:22:54 +0100

    Fix schema change

--- pyvat/registries.py
+++ pyvat/registries.py
@@ -1,7 +1,11 @@
 import requests
 import xml.dom.minidom
+
+from requests import Timeout
+
 from .result import VatNumberCheckResult
-from .xml_utils import get_first_child_element, get_text
+from .xml_utils import get_first_child_element, get_text, NodeNotFoundError
+from .exceptions import ServerError
 
 
 class Registry(object):
@@ -23,7 +27,6 @@ class Registry(object):
 
 class ViesRegistry(Registry):
     """VIES registry.
-
     Uses the European Commision's VIES registry for validating VAT numbers.
     """
 
@@ -31,6 +34,9 @@ class ViesRegistry(Registry):
         'services/checkVatService'
     """URL for the VAT checking service.
     """
+
+    DEFAULT_TIMEOUT = 8
+    """Timeout for the requests."""
 
     def check_vat_number(self, vat_number, country_code):
         # Non-ISO code used for Greece.
@@ -41,15 +47,15 @@ class ViesRegistry(Registry):
         result = VatNumberCheckResult()
 
         request_data = (
-            u'<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope'
-            u' xmlns:ns0="urn:ec.europa.eu:taxud:vies:services:checkVa'
-            u't:types" xmlns:ns1="http://schemas.xmlsoap.org/soap/enve'
-            u'lope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-insta'
-            u'nce" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/env'
-            u'elope/"><SOAP-ENV:Header/><ns1:Body><ns0:checkVat><ns0:c'
-            u'ountryCode>%s</ns0:countryCode><ns0:vatNumber>%s</ns0:va'
-            u'tNumber></ns0:checkVat></ns1:Body></SOAP-ENV:Envelope>' %
-            (country_code, vat_number)
+                u'<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope'
+                u' xmlns:ns0="urn:ec.europa.eu:taxud:vies:services:checkVa'
+                u't:types" xmlns:ns1="http://schemas.xmlsoap.org/soap/enve'
+                u'lope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-insta'
+                u'nce" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/env'
+                u'elope/"><SOAP-ENV:Header/><ns1:Body><ns0:checkVat><ns0:c'
+                u'ountryCode>%s</ns0:countryCode><ns0:vatNumber>%s</ns0:va'
+                u'tNumber></ns0:checkVat></ns1:Body></SOAP-ENV:Envelope>' %
+                (country_code, vat_number)
         )
 
         result.log_lines += [
@@ -63,8 +69,13 @@ class ViesRegistry(Registry):
                 data=request_data.encode('utf-8'),
                 headers={
                     'Content-Type': 'text/xml; charset=utf-8',
-                }
+                },
+                timeout=self.DEFAULT_TIMEOUT
             )
+        except Timeout as e:
+            result.log_lines.append(u'< Request to EU VIEW registry timed out:'
+                                    u' {}'.format(e))
+            return result
         except Exception as exception:
             # Do not completely fail problematic requests.
             result.log_lines.append(u'< Request failed with exception: %r' %
@@ -76,11 +87,11 @@ class ViesRegistry(Registry):
             u'< Response with status %d of content type %s:' %
             (response.status_code, response.headers['Content-Type']),
             response.text,
-        ]
+            ]
 
         # Do not completely fail problematic requests.
         if response.status_code != 200 or \
-           not response.headers['Content-Type'].startswith('text/xml'):
+                not response.headers['Content-Type'].startswith('text/xml'):
             result.log_lines.append(u'< Response is nondeterministic due to '
                                     u'invalid response status code or MIME '
                                     u'type')
@@ -91,33 +102,49 @@ class ViesRegistry(Registry):
         # We basically expect the result structure to be as follows,
         # where the address and name nodes might be omitted.
         #
-        # <soap:Envelope>
-        #   <soap:Body>
-        #     <checkVatResponse>
-        #       <countryCode>..</countryCode>
-        #       <vatNumber>..</vatNumber>
-        #       <requestDate>..</requestDate>
-        #       <valid>..</valid>
-        #       <name>..</name>
-        #       <address>..</address>
-        #     </checkVatResponse>
-        #   </soap:Body>
-        # </soap:Envelope>
-        try:
-            result_dom = xml.dom.minidom.parseString(
-                response.text.encode('utf-8')
+        # <env:Envelope
+        #     xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+        #         <env:Header/>
+        #         <env:Body>
+        #         <ns2:checkVatResponse
+        #              xmlns:ns2="urn:ec.europa.eu:taxud:vies:services:checkVat:types">
+        #              <ns2:countryCode>DE</ns2:countryCode>
+        #              <ns2:vatNumber>812383453</ns2:vatNumber>
+        #              <ns2:requestDate>2022-08-12+02:00</ns2:requestDate>
+        #              <ns2:valid>true</ns2:valid>
+        #              <ns2:name>---</ns2:name>
+        #              <ns2:address>---</ns2:address>
+        #         </ns2:checkVatResponse>
+        #     </env:Body>
+        # </env:Envelope>
+        result_dom = xml.dom.minidom.parseString(response.text.encode('utf-8'))
+
+        envelope_node = result_dom.documentElement
+        if envelope_node.tagName != 'env:Envelope':
+            raise ValueError(
+                'expected response XML root element to be a SOAP envelope'
             )
 
-            envelope_node = result_dom.documentElement
-            if envelope_node.tagName != 'soap:Envelope':
-                raise ValueError('expected response XML root element to be a '
-                                 'SOAP envelope')
+        body_node = get_first_child_element(envelope_node, 'env:Body')
 
-            body_node = get_first_child_element(envelope_node, 'soap:Body')
-            check_vat_response_node = \
-                get_first_child_element(body_node, 'checkVatResponse')
-            valid_node = get_first_child_element(check_vat_response_node,
-                                                 'valid')
+        # Check for server errors
+        try:
+            error_node = get_first_child_element(body_node, 'env:Fault')
+            fault_strings = error_node.getElementsByTagName('faultstring')
+            fault_code = fault_strings[0].firstChild.nodeValue
+            raise ServerError(fault_code)
+        except NodeNotFoundError:
+            pass
+
+        try:
+            check_vat_response_node = get_first_child_element(
+                body_node,
+                'ns2:checkVatResponse'
+            )
+            valid_node = get_first_child_element(
+                check_vat_response_node,
+                'ns2:valid'
+            )
         except Exception as e:
             result.log_lines.append(u'< Response is nondeterministic due to '
                                     u'invalid response body: %r' % (e))
@@ -135,17 +162,21 @@ class ViesRegistry(Registry):
 
         # Parse the business name and address if possible.
         try:
-            name_node = get_first_child_element(check_vat_response_node,
-                                                'name')
+            name_node = get_first_child_element(
+                check_vat_response_node,
+                'ns2:name'
+            )
             result.business_name = get_text(name_node).strip() or None
-        except:
+        except Exception:
             pass
 
         try:
-            address_node = get_first_child_element(check_vat_response_node,
-                                                   'address')
+            address_node = get_first_child_element(
+                check_vat_response_node,
+                'ns2:address'
+            )
             result.business_address = get_text(address_node).strip() or None
-        except:
+        except Exception:
             pass
 
         return result
