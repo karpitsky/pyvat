4131dfcba47c95a0a6a66b39aee6e626f8a6b49a
Mike <558554+karpitsky@users.noreply.github.com>
2026-01-15 12:18:42 +0000

    Update Estonia VAT rate to 24%

--- pyvat/vat_rates.py
+++ pyvat/vat_rates.py
@@ -155,6 +155,17 @@ VAT_RATES = {
             ItemType.ebook: Decimal('22.0'),
             ItemType.enewspaper: Decimal('22.0')
         }
+    }, {
+        'valid_from': datetime.date(2025, 7, 1),
+        'rates': {
+            ItemType.generic_physical_good: Decimal('24.0'),
+            ItemType.generic_electronic_service: Decimal('24.0'),
+            ItemType.generic_telecommunications_service: Decimal('24.0'),
+            ItemType.generic_broadcasting_service: Decimal('24.0'),
+            ItemType.prepaid_broadcasting_service: Decimal('24.0'),
+            ItemType.ebook: Decimal('24.0'),
+            ItemType.enewspaper: Decimal('24.0')
+        }
     }],
     'ES': [{
         'valid_from': datetime.date(2012, 9, 1),
